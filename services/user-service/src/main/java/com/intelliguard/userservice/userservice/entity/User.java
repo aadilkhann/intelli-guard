69fdc9a61a4b8f5d79d002bce23c399616b12f86
Adil Khan <khan01970@gmail.com>
2025-10-23 00:59:52 +0530

    Added User Entity

---
+++ services/user-service/src/main/java/com/intelliguard/userservice/userservice/entity/User.java
@@ -0,0 +1,17 @@
+package com.intelliguard.userservice.userservice.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+}
